a4644b4069e339c3be3ae715ce2a8885e9f8d61d
Balaji Anoop Gupta <balajianoopgupta.ba@gmail.com>
2018-03-23 02:09:45 -0700

    Added increment, decrement and selide functions for the stepper and the slider

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,45 @@ import {View} from 'react-native';
 import {getMetricMetaInfo} from '../utils/helpers';
 
 export default class AddEntry extends Component{
+
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0
+  }
+
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric);
+
+    this.setState((currentState) => {
+      const count = state[metric] + step;
+
+      return {
+        ...state,
+        [metric]: count>max ? max : count
+      }
+    })
+  }
+
+  decrement = (metric) => {
+    this.setState((currentState) => {
+      const count = state[metric] - getMetricMetaInfo(metric);
+
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count
+      }
+    })
+  },
+
+  slide = (metric, value) => {
+    this.setState((metric) => ({
+      [metric] = value
+    }))
+  }
+
   render(){
     return(
       <View>
